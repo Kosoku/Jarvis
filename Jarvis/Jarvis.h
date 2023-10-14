00cca2a8fbc2cb24c3fd4d57bc4f3a9adcfd880f
Jason R. Anderson <jason@kosoku.com>
2023-10-14 14:48:18 -0400

    Initial Commit

---
+++ Jarvis/Jarvis.h
@@ -0,0 +1,18 @@
+//
+//  Jarvis.h
+//  Jarvis
+//
+//  Created by Jason Anderson on 10/14/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Jarvis.
+FOUNDATION_EXPORT double JarvisVersionNumber;
+
+//! Project version string for Jarvis.
+FOUNDATION_EXPORT const unsigned char JarvisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jarvis/PublicHeader.h>
+
+
